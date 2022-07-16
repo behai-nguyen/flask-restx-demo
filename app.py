494a8b103671106d91afd948abd0ef4f29be8f28
behai-nguyen <behai_nguyen@hotmail.com>
2022-07-16 11:58:53 +1000

    Added @app.shell_context_processor.

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 """Flask Application entry point."""
 
-from flask_restx_demo import create_app
+from flask_restx_demo import (
+    create_app,
+	db,
+)	
 
 app = create_app()
+
+@app.shell_context_processor
+def shell():
+    return {
+        "db": db,
+    }
